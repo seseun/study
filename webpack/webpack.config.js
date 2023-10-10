6af167140d839639f44ff45b25f8347da0df7892
sese <dev.seseun@gmail.com>
2023-10-10 13:46:32 +0900

    feat: Add webpack-mode
    webpack 모드도입
    https://opentutorials.org/module/4566/27556

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './source/index.js',
   output: {
     path: path.resolve(__dirname, 'public'), // __dirname: webpack.config.js의 위치:: node.js의 약속
