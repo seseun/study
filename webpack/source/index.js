648c20f22ed0d9e4447be3144596ec4fc112b07c
sese <dev.seseun@gmail.com>
2023-10-10 14:31:41 +0900

    feat: Add webpack-npm
    webpack  npm package
    https://opentutorials.org/module/4566/27561

--- webpack/source/index.js
+++ webpack/source/index.js
@@ -1,6 +1,7 @@
 import hellow_word from './hello.js';
 import world_word from './world.js';
+import _ from 'lodash';
 import css from './style.css';
 
-document.querySelector('#root').innerHTML = `${hellow_word} ${world_word}`;
+document.querySelector('#root').innerHTML = _.join([hellow_word, world_word], '⭐');
 console.log('css', css);
