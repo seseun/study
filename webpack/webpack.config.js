d09b2333918428b60febc42b86367165847dc5be
sese <dev.seseun@gmail.com>
2023-10-10 13:20:35 +0900

    feat: Add webpack-config
    webpack 설정도입
    https://opentutorials.org/module/4566/27555

---
+++ webpack/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './source/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'), // __dirname: webpack.config.js의 위치:: node.js의 약속
+    filename: 'index_bundle.js',
+  },
+};
