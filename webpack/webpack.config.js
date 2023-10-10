ead0ff7fcca80c15690f625de587a11366b57f99
sese <dev.seseun@gmail.com>
2023-10-10 14:27:29 +0900

    feat: Add webpack-plugin
    webpack plugin 도입
    https://opentutorials.org/module/4566/27559

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -18,4 +19,16 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './source/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './source/about.html',
+      filename: 'about.html',
+      chunks: ['about'],
+    }),
+  ],
 };
